142cb5a03cd1030dcc3c4c65c9aadf99414d77b2
franklinfollis <francis.follis96@gmail.com>
2016-03-14 15:04:41 +0800

    Search only conducts if query has characters

--- search.js
+++ search.js
@@ -1,5 +1,9 @@
 function Search(){
-  var querystring = "http://www.google.com/search?q=" + encodeURIComponent($('#search').val());
-  window.location.replace(querystring);
+  var querystring =  $('#search').val();
+  if(querystring.trim()){
+    var urlquery = "http://www.google.com/search?q=" + encodeURIComponent(querystring);
+    window.location.replace(urlquery);
+    return false;
+  }
   return false;
 }
