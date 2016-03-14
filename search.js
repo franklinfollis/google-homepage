d6ea8e6e5144063d50577805a154f471055937a2
franklinfollis <francis.follis96@gmail.com>
2016-03-14 18:39:16 +0800

    Made the I'm feeling lucky redirect to wiki page

--- search.js
+++ search.js
@@ -2,8 +2,13 @@ function Search(){
   var querystring =  $('#search').val();
   if(querystring.trim()){
     var urlquery = "http://www.google.com/search?q=" + encodeURIComponent(querystring);
-    window.location.replace(urlquery);
+    window.location.href = urlquery;
     return false;
   }
+  return false;
+}
+
+function feelinglucky(){
+  window.location.href = "https://en.wikipedia.org/wiki/Google_Search#.22I.27m_Feeling_Lucky.22";
   return false;
 }
